0d53fa4268816c0342cf697f9399db5eee622ddc
chenshuai15 <chenshuai15@126.com>
2016-07-11 23:37:56 +0800

    quartz 示例提交
    Change-Id: I03a7fe947baeaede55065d37697c9828231a7c36
    Signed-off-by: chenshuai15 <chenshuai15@126.com>

---
+++ example/src/main/java/com/chen/quartz/task/MyJob.java
@@ -0,0 +1,16 @@
+package com.chen.quartz.task;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class MyJob implements Job{
+	
+	@Override
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+		System.out.println("Hello quzrtz  "+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(new Date()));
+	}
+}
