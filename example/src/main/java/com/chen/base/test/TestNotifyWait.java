42800d4ab03e8e8a87aab89ec751338002c48316
chenshuai15 <chenshuai15@126.com>
2016-08-08 17:02:41 +0800

    线程间通过wait notify进行通信 例子中实现功能为对一个整数 2个线程依次进行累加
    Change-Id: Ifb7a733653955e75a77a76a4da38b6d3b3bc8b9c
    Signed-off-by: chenshuai15 <chenshuai15@126.com>

---
+++ example/src/main/java/com/chen/base/test/TestNotifyWait.java
@@ -0,0 +1,52 @@
+package com.chen.base.test;
+
+/**
+ * 线程间通过wait notify进行通信
+ * <p>
+ * 
+ * 例子中实现功能为对一个整数 2个线程依次进行累加
+ * @author heyi
+ *
+ */
+public class TestNotifyWait {
+	
+	int i = 0;
+	
+	public synchronized void increment(){
+		System.out.println("thread:" + Thread.currentThread().getName() + " i:" + i ++);
+		try {
+			this.notify();
+			this.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void main(String[] args){
+		final TestNotifyWait num = new TestNotifyWait();
+		
+		Thread threadA = new Thread("A"){
+			@Override
+			public void run() {
+				for(int i = 0; i < 50; i ++){
+					num.increment();
+				}
+			}
+			
+		};
+		
+		Thread threadB = new Thread("B") {
+			@Override
+			public void run() {
+				for(int i = 0; i < 50; i ++){
+					num.increment();
+				}
+			}
+		};
+		
+		threadA.start();
+		threadB.start();
+	}
+	
+}
