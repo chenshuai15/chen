6dd5aa33f550e432c4c9ce55a60665c88cd636e8
chenshuai15 <chenshuai15@126.com>
2016-10-30 23:26:43 +0800

    泛型测试
    Change-Id: If5ca0617554ad77b0c8fc951625186d57fd190dc
    Signed-off-by: chenshuai15 <chenshuai15@126.com>

--- example/src/test/java/com/mvc/test/Test.java
+++ example/src/test/java/com/mvc/test/Test.java
@@ -1,10 +1,31 @@
 package com.mvc.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		unsafeAdd(new ArrayList<String>());
+		
+	}
+	
+	static void unsafeAdd(List<?> list ){
+		list.add(null);
+//		list.add("aaa"); 报错，无限制的通配符类型 不允许add除null以外的任何元素
+		
+		List a = new ArrayList();
+		a.add(Integer.valueOf(1));
+		a.add("sss");
+		
+		//报错
+//		if(a instanceof List<String>)
+//			List<?> b = a;
+//		}
+		
+		if(a instanceof List<?>){
+			List<?> b = a;
+		}
 	}
 
 }
